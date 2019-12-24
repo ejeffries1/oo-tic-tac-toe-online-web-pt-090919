72b7f1a2c59cb5d03b6ad59f4fa830d3f7466519
Evan Jeffries <ejeffries14@gmail.com>
2019-12-24 22:25:15 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,77 @@
+require 'pry'
+class TicTacToe
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  def initialize
+    @board = Array.new(9, " ") # Array.new(9, " ") = Array = [" "," "," "," "," "," "," "," "," "]
+  end
+
+  def display_board
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts "-----------"
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts "-----------"
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(index, token = "X")
+    @board[index] = token
+  end
+
+  def position_taken?(index)
+    if @board[index] == " "
+      false
+    else
+      true
+    end
+  end
+
+  def valid_move?(index)
+    if @board[index] == " "
+      true
+    else position_taken?(index) == false
+      false || nil
+    end
+  end
+
+  def turn_count
+    @board.count do |i|
+      i == "O" || i == "X"
+    end
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def turn
+    #puts "Please enter input: 1-9"
+    input = gets.chomp
+    index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+    else !valid_move?
+      turn
+    end
+    display_board
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |combo|
+      
+    end
+  end
+end
